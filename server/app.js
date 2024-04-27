58bcd001e55902558ecbe8e34dc3edec42bd6027
nvdai1506 <nvdai1506@gmail.com>
2024-04-27 10:16:54 +0700

    Add https whitelist

--- server/app.js
+++ server/app.js
@@ -1,23 +1,23 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
-import express from 'express';
-import morgan from 'morgan';//logger
-import helmet from 'helmet';//for header security
-import mongoose from 'mongoose';
+import express from "express";
+import morgan from "morgan"; //logger
+import helmet from "helmet"; //for header security
+import mongoose from "mongoose";
 import cors from "cors";
-import { v4 } from 'uuid';
-import multer from 'multer';//serve for files
+import { v4 } from "uuid";
+import multer from "multer"; //serve for files
 
-import AdminRouter from './routes/admin.route.js';
-import AuthRouter from './routes/auth.route.js';
-import UserRouter from './routes/user.route.js';
-import ShopRouter from './routes/shop.route.js';
-import PaymentRouter from './routes/payment.route.js';
-import NotificationRouter from './routes/notification.route.js';
-import Auth from './middlewares/auth.mdw.js';
+import AdminRouter from "./routes/admin.route.js";
+import AuthRouter from "./routes/auth.route.js";
+import UserRouter from "./routes/user.route.js";
+import ShopRouter from "./routes/shop.route.js";
+import PaymentRouter from "./routes/payment.route.js";
+import NotificationRouter from "./routes/notification.route.js";
+import Auth from "./middlewares/auth.mdw.js";
 
 // import helper from './utils/helper.js';
 // import Order from './models/order.model.js';
@@ -29,7 +29,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
-const test = dotenv.config({ path: __dirname + '/.env' });
+const test = dotenv.config({ path: __dirname + "/.env" });
 // console.log(test);
 
 // app.use(cors({
@@ -38,19 +38,27 @@ const test = dotenv.config({ path: __dirname + '/.env' });
 // }));
 // const whitelist = ["http://localhost:3000"];
 // const whitelist = ["http://localhost:3000","https://nv-dai.com","https://admin.nv-dai.com"];
-const whitelist = ["http://localhost:3000","http://nv-dai.com","http://admin.nv-dai.com"];
+const whitelist = [
+  "http://localhost:3000",
+  "http://nv-dai.com",
+  "http://admin.nv-dai.com",
+  "https://nv-dai.com",
+  "https://admin.nv-dai.com",
+];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      console.warn(`Origin '${origin}' is not in the white list:`, whitelist);
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        console.warn(`Origin '${origin}' is not in the white list:`, whitelist);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -65,20 +73,19 @@ app.use(helmet());
 //   next();
 // });
 
-
 const fileStorage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'images');
+    cb(null, "images");
   },
   filename: function (req, file, cb) {
-    cb(null, v4() + '-' + file.originalname)
-  }
+    cb(null, v4() + "-" + file.originalname);
+  },
 });
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
   ) {
     cb(null, true);
   } else {
@@ -86,19 +93,17 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-
-
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use('/', AuthRouter);
-app.use('/shop', ShopRouter);
-app.use('/payment', PaymentRouter);
-app.use('/notification', NotificationRouter);
-app.use('/admin', Auth, AdminRouter);
-app.use('/user', Auth, UserRouter);
+app.use("/", AuthRouter);
+app.use("/shop", ShopRouter);
+app.use("/payment", PaymentRouter);
+app.use("/notification", NotificationRouter);
+app.use("/admin", Auth, AdminRouter);
+app.use("/user", Auth, UserRouter);
 
 app.use((error, req, res, next) => {
   console.log(error.message);
@@ -108,26 +113,28 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data });
 });
 
-app.get('/hello', (req, res, next) => {
+app.get("/hello", (req, res, next) => {
   res.json({ mess: "hello from nodejs" });
-})
+});
 
 // pushNotification('all', 'test title', 'message test', 'tag test');
 
 const PORT = process.env.PORT || 8080;
-console.log('process.env.MONGODB_URI:', process.env.MONGODB_URI)
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(result => {
-  // helper.resetRate();
-  // helper.addPaymentMethodToShippingInfo();
-  // helper.addPropertyToModel('percent', 0, Order);
-  // helper.updateTotalOrder();
-  // const testn = new Notification({ topic: 'test', subscriptions: [] });
-  // await testn.save();
-  app.listen(PORT, function () {
-    console.log(`Server is listening at http://localhost:${PORT}`);
-  });
-}).catch(err => console.log(err));
-
+console.log("process.env.MONGODB_URI:", process.env.MONGODB_URI);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    // helper.resetRate();
+    // helper.addPaymentMethodToShippingInfo();
+    // helper.addPropertyToModel('percent', 0, Order);
+    // helper.updateTotalOrder();
+    // const testn = new Notification({ topic: 'test', subscriptions: [] });
+    // await testn.save();
+    app.listen(PORT, function () {
+      console.log(`Server is listening at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
